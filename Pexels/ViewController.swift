612fe26a77e7179446bd0488f908b5b487d10ad1
Genki <fg2920@yoshida-dental.co.jp>
2021-12-16 11:20:21 +0900

    Initial Commit

---
+++ Pexels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pexels
+//
+//  Created by GENKI Mac on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
