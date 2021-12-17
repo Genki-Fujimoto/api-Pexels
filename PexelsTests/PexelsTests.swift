8296786f34d5335be1453e5dd82cef06f41035cf
Genki <fg2920@yoshida-dental.co.jp>
2021-12-17 21:05:42 +0900

    テストコード修正

--- PexelsTests/PexelsTests.swift
+++ PexelsTests/PexelsTests.swift
@@ -10,13 +10,22 @@ import XCTest
 
 class PexelsTests: XCTestCase {
     
-    //検索結果の個数確認テスト
-    func ResultCount(){
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+    //検索結果の個数の確認
+    func ResultCount() throws {
         
         let ApiList = ApiListModel()
         let expect = expectation(description: "SendMyRequest")
         
-        ApiList.searchEvents(keyword: "犬", success: {(api) in
+        ApiList.searchEvents(keyword: "", success: {(api) in
             
             let result = api.photos.count
             print(result)
@@ -40,5 +49,14 @@ class PexelsTests: XCTestCase {
         }
     }
     
+    
+    /*
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    */
     
 }
